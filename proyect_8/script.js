b85cbe2733c68cc071e8eb5b5a1029a89431b931
Jesner <jesner631@gmail.com>
2022-12-06 10:59:51 -0500

    ok

---
+++ proyect_8/script.js
@@ -0,0 +1,24 @@
+const sound = ['Arrow', 'Chicken', 'Heavy', 'Laser', 'Lion', 'Thunder']
+
+sound.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sound.forEach(sound => {
+        const song = document.getElementById(sound)
+        
+        song.pause()
+        song.currentTime = 0
+    })
+}
---
+++ proyect_9/script.js
@@ -0,0 +1,20 @@
+const jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+const config ={headers: {'Accept':'application/json'}}
+
+generateJoke ()
+
+async function generateJoke() {
+    const res = await fetch('https://icanhazdadjoke.com', config)
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+}
+
+// function generateJoke() {
+//     fetch('https://icanhazdadjoke.com', config)
+//         .then(res => res.json())
+//         .then(data => {jokeEl.innerHTML = data.joke})
+// }
