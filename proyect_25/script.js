9b3801c77581d9098f7ba592c937dc84d17936e5
JesnerW <jesner631@gmail.com>
2022-12-15 16:39:32 -0500

    Boton con notificaciones chidas

---
+++ proyect_25/script.js
@@ -0,0 +1,27 @@
+const button = document.getElementById('button')
+const toasts = document.getElementById('toasts')
+
+const message = ['Message One', 'Message Two', 'Message Three', 'Message Four']
+
+const types = ['info', 'success', 'error']
+
+button.addEventListener('click', () => createNotification())
+
+function createNotification() {
+  const notif = document.createElement('div')
+  notif.classList.add('toast')
+  notif.classList.add(getRandomType())
+  notif.innerText = getRandomMessage()
+  toasts.appendChild(notif)
+  setTimeout(() => {
+    notif.remove()
+  }, 3000)
+}
+
+function getRandomMessage() {
+  return message[Math.floor(Math.random() * message.length)]
+}
+
+function getRandomType() {
+  return types[Math.floor(Math.random() * types.length)]
+}
